e351196948430807a986ef46197bb2deffcf75b1
Komal Thakur <183916252+KomalThakur204@users.noreply.github.com>
2025-05-12 12:28:48 +0530

    Deleted 1.py

--- 1.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-
-root = tk.Tk()
-screen_width = root.winfo_screenwidth()  # Get screen width
-screen_height = root.winfo_screenheight()  # Get screen height
-
-print(f"Screen Width: {screen_width}, Screen Height: {screen_height}")
-
-root.mainloop()
